c19b73bea9433148cef4edfffba39b743b676aeb
multiihub <bhattisaaab007@gmail.com>
2026-07-12 21:00:38 -0700

    Update series-player.js

--- series-player.js
+++ series-player.js
@@ -1,32 +1,17 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { db } from "./firebase.js";
 
 import {
-    getFirestore,
     doc,
     getDoc
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 
-const firebaseConfig = {
-    apiKey: "AIza16TuLrKEYh_SjImYvd99pujHR00zDZ8",
-    authDomain: "mulltibox.firebaseapp.com",
-    projectId: "mulltibox",
-    storageBucket: "mulltibox.firebasestorage.app",
-    messagingSenderId: "197181870596",
-    appId: "1:197181870596:web:c65ecfa9853ee4849f5b51"
-};
+const params = new URLSearchParams(
+    window.location.search
+);
 
 
-const app = initializeApp(firebaseConfig);
-
-const db = getFirestore(app);
-
-
-
-const params = new URLSearchParams(window.location.search);
-
-const seriesId = params.get("id");
-
+const episodeId = params.get("id");
 
 
 const title = document.getElementById("seriesTitle");
@@ -35,91 +20,54 @@ const video = document.getElementById("seriesVideo");
 
 const description = document.getElementById("seriesDescription");
 
-const episodesBox = document.getElementById("episodes");
+
+
+async function loadEpisode(){
+
+
+    if(!episodeId){
+
+        title.textContent = "Episode not found";
+
+        return;
+
+    }
 
 
 
-
-async function loadSeries(){
-
-
-    try{
+    const episodeRef = doc(db,"episodes",episodeId);
 
 
-        const ref = doc(db,"series",seriesId);
-
-        const snap = await getDoc(ref);
+    const snap = await getDoc(episodeRef);
 
 
 
-        if(snap.exists()){
+    if(snap.exists()){
 
 
-            const series = snap.data();
-
-
-
-            title.textContent = series.title;
-
-
-            description.textContent =
-            series.description || "No description available.";
+        const episode = snap.data();
 
 
 
-            series.episodes.forEach((episode,index)=>{
+        title.textContent = episode.title;
 
 
-                let button = document.createElement("button");
-
-
-                button.textContent =
-                "Episode " + (index + 1);
+        description.textContent =
+        episode.description || "";
 
 
 
-                button.onclick = ()=>{
-
-
-                    video.src = episode;
-
-
-                    video.play();
-
-
-                };
-
-
-
-                episodesBox.appendChild(button);
-
-
-
-            });
-
-
-
-            video.src = series.episodes[0];
-
-
-
-        }
-        else{
-
-
-            title.textContent="Series not found.";
-
-
-        }
+        video.src = episode.videoUrl;
 
 
 
     }
-    catch(error){
+    else{
 
-        console.error(error);
 
-        title.textContent="Error loading series.";
+        title.textContent =
+        "Episode not found";
+
 
     }
 
@@ -128,4 +76,5 @@ async function loadSeries(){
 }
 
 
-loadSeries();
+
+loadEpisode();
